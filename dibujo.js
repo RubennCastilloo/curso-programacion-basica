883a903e133e5be5f573df73eddb7bbba2387f8b
Ruben Castillo <rcastillo@raivtech.com>
2019-07-13 15:55:48 -0600

    Eventos y Formularios en HTML y JavaScript
    Los eventos son funciones que suceden cuando algo ocurre,  sucesos cómo un click, pulsar una tecla, colocar el mouse sobre un botón, etc.
    
    Creemos un programa que reciba la cantidad de líneas con las que queremos crear nuestra imagen.
    
    Recuerda:
    
    Cuando queremos enviar información a un servidor podemos usar un formularios
    
    En JavaScript puedes agregar un manejador de eventos con element.addEventListener('event', function)

--- dibujo.js
+++ dibujo.js
@@ -1,39 +1,12 @@
+var texto = document.getElementById("texto-linea");
+var boton = document.getElementById('botoncito');
+
+boton.addEventListener('click', dibujarPorClick);
+
 var d = document.getElementById('dibujito');
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var colorcito = 'red';
 
-// while (l < lineas) {
-//     yi = 10 * l
-//     xf = 10 * (l + 1);
-
-//     dibujarLinea(colorcito, 0, yi, xf,300);
-//     console.log("linea" + l);
-//     l++;
-// }
-
-for (l=0; l < lineas; l++) {
-    yi = 10 * l
-    xf = 10 * (l + 1);
-
-    dibujarLinea(colorcito, 0, yi, xf,300);
-    // dibujarLinea(colorcito, 300, yi, xf, 0);
-    console.log("linea" + l);
-    
-}
-
-for (l=0; l < lineas; l++) {
-    yi = 300 - (10 * l);
-    xf = 10 * (l+1);
-    // dibujarLinea(colorcito, 300, yi, xf, 300);
-    dibujarLinea(colorcito, 0, yi, xf, 0);
-    
-}
-
-dibujarLinea(colorcito, 1, 1, 1, 300);
-dibujarLinea(colorcito, 1, 299, 299, 300);
 
 
 
@@ -47,4 +20,43 @@ lienzo.moveTo(xinicial, yinicial);
 lienzo.lineTo(xfinal, yfinal);
 lienzo.stroke();
 lienzo.closePath();
+}
+
+function dibujarPorClick() {
+        var lineas = parseInt(texto.value);
+        var l = 0;
+        var yi, xf;
+        var colorcito = 'red';
+        var espacio = ancho / lineas;
+
+        // while (l < lineas) {
+        //     yi = 10 * l
+        //     xf = 10 * (l + 1);
+
+        //     dibujarLinea(colorcito, 0, yi, xf,300);
+        //     console.log("linea" + l);
+        //     l++;
+        // }
+
+        for (l=0; l < lineas; l++) {
+            yi = espacio * l
+            xf = espacio * (l + 1);
+
+            dibujarLinea(colorcito, 0, yi, xf,300);
+            // dibujarLinea(colorcito, 300, yi, xf, 0);
+            console.log("linea" + l);
+            
+            
+        }
+
+        for (l=0; l < lineas; l++) {
+            yi = 300 - (espacio * l);
+            xf = espacio * (l+1);
+            // dibujarLinea(colorcito, 300, yi, xf, 300);
+            dibujarLinea(colorcito, 0, yi, xf, 0);
+            
+        }
+
+        dibujarLinea(colorcito, 1, 1, 1, 300);
+        dibujarLinea(colorcito, 1, 299, 299, 300);
 }
